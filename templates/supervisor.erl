5995fa36087e9766ae7029541e37a75d8f330d33
Michael Fagan <mfagan@shoretel.com>
2015-03-12 16:40:55 +1100

    supervisor template

--- templates/supervisor.erl
+++ templates/supervisor.erl
@@ -2,36 +2,52 @@
 -module({{supid}}).
 -behaviour(supervisor).
 
--export([start_link/1, init/1]).
+-export([start_link/0, start_link/1, init/1]).
 
 
 
 %%%%% ------------------------------------------------------- %%%%%
 % Helper macro for declaring children of supervisor
 
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
--define(CHILD(I, Type, Args), {I, {I, start_link, Args}, permanent, 5000, Type, [I]}).
+-define(CHILD(I), {I, {I, start_link, []}, permanent, 5000, worker, [I]}).
+-define(CHILD(I, Args), {I, {I, start_link, Args}, permanent, 5000, worker, [I]}).
+-define(WORKER(I), {I, {I, start_link, []}, permanent, brutal_kill, worker, [I]}).
+-define(SUPERVISOR(I), {I, {I, start_link, []}, permanent, 5000, supervisor, [I]}).
+-define(SUPERVISOR(I, Args), {I, {I, start_link, Args}, permanent, 5000, worker, [I]}).
+
+
+-define(WORKER_POOL(I, R, P), { {simple_one_for_one, R, P}, [?WORKER(I)] }).
+
 
 
 %%%%% ------------------------------------------------------- %%%%%
 % Public API
 
--spec start_link(term())->{ok,pid()}|ignore|{error,any()}.
-start_link(Args) ->
-    supervisor:start_link(?MODULE, Args).
+
+start_link() ->
+    supervisor:start_link({local, {{supid}} }, ?MODULE, {{supid}}).
+    
+    
+start_link({{supid}}2) ->
+    supervisor:start_link({local, {{supid}}_name }, ?MODULE, {{supid}}_pool).    
+
 
     
 %%%%% ------------------------------------------------------- %%%%%
 % Initialise Supervisor
     
-init(_Args) ->
-    Restart = {one_for_one, 2, 5},
-    C0 = { arbitrary_internal_name_term
-         , {mod,func,args}
-         , permanent
-         , 200 % ms
-         , worker
-         , [mod]
-         },
-    {ok,{Restart,[C0]}}.
+
+init({{supid}}_pool) ->
+    { ok, ?WORKER_POOL(replace_with_child_module, 2, 5) }.
     
+    
+init({{supid}}) ->
+    { ok
+    , { {one_for_one, 2, 5}
+      , [ ?CHILD(replace_with_child_module)
+        , ?CHILD(replace_with_child2_module)
+        ]
+      }
+    }.
+    
+
