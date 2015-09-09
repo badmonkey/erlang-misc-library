e149bcb70657eace3c685f2a416f338f34ac4218
Michael Fagan <mfagan@shoretel.com>
2015-09-09 16:50:15 +1000

    Added copy_if_large_ref  try and detach from a large binary reference.

--- src/xbinary.erl
+++ src/xbinary.erl
@@ -3,7 +3,19 @@
 -extends(binary).
 
 
-%-export([]).
+-export([copy_if_large_ref/1]).
 
 
 %%%%% ------------------------------------------------------- %%%%%
+
+
+copy_if_large_ref(Binary) ->
+	case binary:referenced_byte_size(Binary) of
+		Large	when Large > 2 * byte_size(Binary)  ->
+			binary:copy(Binary)
+			
+	;	_ 		-> Binary
+	end.
+	
+	
+
