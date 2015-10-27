77dce66c02aa0328ca41ac521eebe998b0044bef
Michael Fagan <michael@hivemind.darktech.org>
2015-10-27 23:48:21 +1100

    add xets:foreach()

--- src/xets.erl
+++ src/xets.erl
@@ -2,7 +2,7 @@
 -module(xets).
 -extends(ets).
 
--export([match_delete/2]).
+-export([match_delete/2, foreach/2]).
 
 
 
@@ -15,4 +15,19 @@ match_delete(Table, Spec) ->
     [ ets:delete_object(Table, X) || X <- ets:match_object(Table, Spec) ],
     true.
 
+
+%%%%% ------------------------------------------------------- %%%%%
+
     
+-spec foreach( fun((_) -> term()), ets:tab() ) -> ok.
+
+foreach(Fun, Table) ->
+    ets:foldl(
+          fun(X, undefined) ->
+            Fun(X),
+            undefined
+          end
+        , undefined
+        , Table),
+    ok.
+        
