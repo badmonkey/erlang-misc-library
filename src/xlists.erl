d5adbad4ba608037da2dea0695e5fb46c463bb83
Michael Fagan <mfagan@shoretel.com>
2015-10-27 16:17:31 +1100

    Added (but not tested) list shuffle()

--- src/xlists.erl
+++ src/xlists.erl
@@ -4,7 +4,7 @@
 
 -export([ sorted_member/2, sorted_insert/2, subsets/2, unique/1
         , drop/2, take/2, foldl/2, filter_fold/2, filter_fold/3
-        , mapx/3, minmax/1, minmax/2]).
+        , mapx/3, minmax/1, minmax/2, interval/2, shuffle/1, shuffle2/1]).
 
 
 
@@ -141,4 +141,49 @@ minmax(F, [H | Rest], {Min, Max} = Res) ->
                     end
             end,
     minmax(F, Rest, Acc).
+
+
+%%%%% ------------------------------------------------------- %%%%%
+
+
+-spec interval( integer(), integer() ) -> [integer()].
+
+interval(X, X) -> [X];
+interval(Fir, Sec) when Fir < Sec  -> lists:seq(Fir, Sec);
+interval(Fir, Sec) -> lists:seq(Fir, Sec, -1).
+	
+
+%%%%% ------------------------------------------------------- %%%%%
+
+
+-spec shuffle( [T] ) -> [T].
+
+shuffle(L) when is_list(L) -> shuffle(L, []).
+
+
+shuffle([], Acc) -> Acc;
+shuffle([X], Acc) -> [X | Acc];
+shuffle(List, Acc) ->
+	N = length(List),
+	{Leading, [H, T]} = lists:split(random:uniform(N) - 1, List),
+	{Leading, H, T, Leading ++ T}.
+%	shuffle( Leading ++ T, [H | Acc]).
+	
+
+shuffle2(L) ->
+	shuffle2(length(L), L, [], [], 0).
+
+
+shuffle2(0, [], [], Acc, Steps) -> {Acc, Steps};
+shuffle2(N, [], Over, Acc, Steps) -> shuffle2(N, Over, [], Acc, Steps);
+shuffle2(1, [X], [], Acc, Steps) -> shuffle2(0, [], [], [X | Acc], Steps + 1);
+shuffle2(N, [H | T], Over, Acc, Steps) ->
+	case random:uniform(N) of
+		1	-> shuffle2(N - 1, T, Over, [H | Acc], Steps + 1)
+	;	_ 	-> shuffle2(N, T, [H | Over], Acc, Steps + 1)
+	end.
+		
+
+
+
 
